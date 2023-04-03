347be70107ec8f89c5dcd192e115655a49b182a3
Waitman <ya.trofimow2013@yandex.ru>
2023-04-03 16:35:51 +0300

    Фронтенд часть

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchUserData = createAsyncThunk('auth/fetchUserData', async (params) => {
+  const { data } = await axios.post('/auth/login', params);
+  return data;
+});
+
+export const fetchRegister = createAsyncThunk('auth/fetchRegister', async (params) => {
+  const { data } = await axios.post('/auth/register', params);
+  return data;
+});
+
+export const fetchUserDataMe = createAsyncThunk('auth/fetchUserDataMe', async () => {
+  const { data } = await axios.get('/auth/me');
+  return data;
+});
+
+const initialState = {
+  data: null,
+  status: 'loading',
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
+  extraReducers: {
+    [fetchUserData.pending]: (state) => {
+      state.status = 'loading';
+      state.data = null;
+    },
+    [fetchUserData.fulfilled]: (state, action) => {
+      state.status = 'loaded';
+      state.data = action.payload;
+    },
+    [fetchUserData.rejected]: (state) => {
+      state.status = 'error';
+      state.data = null;
+    },
+    [fetchUserDataMe.pending]: (state) => {
+      state.status = 'loading';
+      state.data = null;
+    },
+    [fetchUserDataMe.fulfilled]: (state, action) => {
+      state.status = 'loaded';
+      state.data = action.payload;
+    },
+    [fetchUserDataMe.rejected]: (state) => {
+      state.status = 'error';
+      state.data = null;
+    },
+    [fetchRegister.pending]: (state) => {
+      state.status = 'loading';
+      state.data = null;
+    },
+    [fetchRegister.fulfilled]: (state, action) => {
+      state.status = 'loaded';
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state) => {
+      state.status = 'error';
+      state.data = null;
+    },
+  },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
